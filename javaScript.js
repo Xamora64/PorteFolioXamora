ffe12dea668dbabbebcf4533d01010f0fa9a723e
Xamora64 <49618907+Xamora64@users.noreply.github.com>
2021-02-15 20:22:54 +0100

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -2,17 +2,17 @@ var imagePresent=0;
 function imageChangementPlus() {
 	if (imagePresent==0){
 		imagePresent=1;
-		imageJeu.setAttribute("src","Plateformer2.jpg");
+		imageJeu.setAttribute("src","Plateformer2.JPG");
 		document.getElementById("numeroImageJs").innerHTML = imagePresent + 1;
 	}
 	else if (imagePresent==1){
 		imagePresent=2;
-		imageJeu.setAttribute("src","Plateformer3.jpg");
+		imageJeu.setAttribute("src","Plateformer3.JPG");
 		document.getElementById("numeroImageJs").innerHTML = imagePresent + 1;
 	}
 	else if (imagePresent==2){
 		imagePresent=0;
-		imageJeu.setAttribute("src","Plateformer1.jpg");
+		imageJeu.setAttribute("src","Plateformer1.JPG");
 		document.getElementById("numeroImageJs").innerHTML = imagePresent + 1;
 	}
 }
@@ -22,17 +22,17 @@ imageBoutonPlus.addEventListener("click",imageChangementPlus);
 function imageChangementMoins() {
 	if (imagePresent==0){
 		imagePresent=2;
-		imageJeu.setAttribute("src","Plateformer3.jpg");
+		imageJeu.setAttribute("src","Plateformer3.JPG");
 		document.getElementById("numeroImageJs").innerHTML = imagePresent + 1;
 	}
 	else if (imagePresent==1){
 		imagePresent=0;
-		imageJeu.setAttribute("src","Plateformer1.jpg");
+		imageJeu.setAttribute("src","Plateformer1.JPG");
 		document.getElementById("numeroImageJs").innerHTML = imagePresent + 1;
 	}
 	else if (imagePresent==2){
 		imagePresent=1;
-		imageJeu.setAttribute("src","Plateformer2.jpg");
+		imageJeu.setAttribute("src","Plateformer2.JPG");
 		document.getElementById("numeroImageJs").innerHTML = imagePresent + 1;
 	}
 }
